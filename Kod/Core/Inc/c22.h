8ac6b1e319a1b758026352727776c1818bbed7c1
Michał Papierzański <278354@student.pwr.edu.pl>
2025-11-05 13:55:57 +0100

    Stworzenie biblioteki do obsługi głośnika

---
+++ Kod/Core/Inc/c22.h
@@ -0,0 +1,9 @@
+#include "main.h"
+#ifndef INC_C22_H_
+#define INC_C22_H_
+#define AUDIO_I2C_ADDR	0x94
+
+void cs43l22_write(I2C_HandleTypeDef *hi2c,uint8_t reg, uint8_t value);
+void cs43l22_init(I2C_HandleTypeDef *hi2c);
+
+#endif
